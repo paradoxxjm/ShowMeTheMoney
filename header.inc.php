0ed77b94cc2abee8a06b5746af301d2a4eb3a715
Serign Jobe <serignjobe@gmail.com>
2012-06-05 15:54:09 -0400

    turned the curl get url content code into a method and replaced the other instances of file_get_contents with it

--- header.inc.php
+++ header.inc.php
@@ -7,26 +7,24 @@ const BORROWER_COUNTRY_CODE_INDEX = 12;
 const SUPPLIER_COUNTRY_INDEX = 24;
 const TOTAL_CONTRACT_AMOUNT_INDEX = 25;
 
+function get_url_content($url){
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_FAILONERROR,0 );
+  curl_setopt($ch, CURLOPT_URL, $url);
+  $contents = curl_exec($ch);
+  curl_close ($ch);
+
+  return $contents;
+}
 
 $jsonurl = "https://finances.worldbank.org/api/views/kdui-wcs3/rows.json?search=".urlencode($country_codes[$country_code]);
 
 
-# using Curl instead of file-get.
-#$json = file_get_contents($jsonurl,0,null,null);
-
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_FAILONERROR,0 );
-curl_setopt($ch, CURLOPT_URL, $jsonurl);
-$json = curl_exec($ch);
-curl_close ($ch);
-
-
-$json_output = json_decode($json);
+$json_output = json_decode(get_url_content($jsonurl));
 
 $num_results =  count($json_output->data);
 
@@ -60,7 +58,7 @@ for($i=0; $i<$num_results; $i++){
 
   $supplier_countries = array_unique($supplier_countries);
   $supplier_coordinates = array();
-  $borrower_geo_json =  json_decode(file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($country_codes[$country_code])."&sensor=false"));
+  $borrower_geo_json =  json_decode(get_url_content("https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($country_codes[$country_code])."&sensor=false"));
   $borrower_cordinates = array(
         "lat"=>$borrower_geo_json->results[0]->geometry->location->lat,
         "lng"=>$borrower_geo_json->results[0]->geometry->location->lng);
@@ -68,7 +66,7 @@ for($i=0; $i<$num_results; $i++){
   $flipped = array_flip($country_codes);
 
   foreach($supplier_countries as $country){
-    $geo_json_url = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($country)."&sensor=false");
+    $geo_json_url = get_url_content("https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($country)."&sensor=false");
     $geo_json = json_decode($geo_json_url);
     $supplier_coordinates[] = array(
         $geo_json->results[0]->geometry->location->lng,
